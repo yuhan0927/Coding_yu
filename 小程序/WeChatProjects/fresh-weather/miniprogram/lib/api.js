20ab56d7f09fd462a7c7ff89f17b03eb2fa86059
yuhan0927 <1138767969@qq.com>
2019-05-16 20:46:26 +0800

    天气小程序

---
+++ 小程序/WeChatProjects/fresh-weather/miniprogram/lib/api.js
@@ -0,0 +1,44 @@
+const QQ_MAP_KEY = 'OBNBZ-POSWX-GDP4N-ZQUW7-VZHXJ-BGBZV'
+
+wx.cloud.init({
+    env:'yh0927'
+})
+
+const db = wx.cloud.database()
+
+// 添加心情
+export const addEmotion = (openid,emotion) => {
+    return db.collection('diary').add({
+        data: {
+        openid,
+        emotion,
+        tsModified:Date.now()
+        }
+      
+    })
+}
+
+// 获取位置
+export const geocoder = (lat,lon,success = () =>{},fail = () => {}) =>{
+    return wx.request({
+        url:'https://apis.map.qq.com/ws/geocoder/v1/',
+        data:{
+            location:`${lat},${lon}`,
+            key:QQ_MAP_KEY,
+            get_pio:0
+        },
+        success,
+        fail
+    })
+}
+
+// 获取天气
+export const getWeather = (lat,lon)=>{
+    return wx.cloud.callFunction({
+        name:'he-weather',
+        data:{
+            lat,
+            lon
+        }
+    })
+}
