2036d8b268a92a242b6d1b6943359817973fe6df
yuhan0927 <1138767969@qq.com>
2019-05-20 19:08:04 +0800

    小程序

---
+++ 小程序/WeChatProjects/sg/config/api.js
@@ -0,0 +1,5 @@
+export const API_BASE = 'https://www.easy-mock.com/mock/5cc27bf30b08a528bd62e84e/llmhahaha/gdhfjdf'
+export const API_DETAIL = 'https://www.easy-mock.com/mock/5cc27f3a8ea8b86f49399001/fgffg/hg'
+export const API_CREATE = 'https://www.easy-mock.com/mock/5af03ac5957fd4308a30b9c0/api/create#!method=get'
+export const API_DELETE_IMG = 'https://www.easy-mock.com/mock/5af03ac5957fd4308a30b9c0/api/delete_img#!method=delete'
+export const API_LOGIN = 'https://www.easy-mock.com/mock/5af03ac5957fd4308a30b9c0/api/login#!method=post'
