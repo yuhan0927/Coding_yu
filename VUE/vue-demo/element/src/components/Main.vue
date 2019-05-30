badcb3f09bb2f21a8b4d8a9f4c21c4d383932244
yuhan0927 <1138767969@qq.com>
2019-05-30 22:31:37 +0800

    node_modules

---
+++ VUE/vue-demo/element/src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class='main-content'>
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from './common/Header'
+export default {
+  name: 'mainContent',
+  data () {
+    return {
+      msg: 'hello world'
+    }
+  },
+  // 声明注册
+  components: {
+    Header
+  }
+}
+</script>
