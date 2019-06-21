88bc234e22ff84ae4b13ba22dc9c36842aae8ba7
yuhan0927 <1138767969@qq.com>
2019-06-21 21:19:08 +0800

    react-router-ant

---
+++ react/react-router-ant/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Layout from './page/Layout';
+// import logo from './logo.svg';
+import 'antd/dist/antd.css';
+// import './App.css';
+
+function Table() {
+  return (
+    <div>Table</div>
+  )
+}
+function Lable() {
+  return (
+    <div>Lable</div>
+  )
+}
+function App() {
+  return (
+    <Router>
+      <Route path="/" component={Layout}>
+        {/*  */}
+        <Route path="/" component={Layout}></Route>
+        <Route path="/table" component={Table}></Route>
+        <Route path="/lable" component={Lable}></Route>
+      </Route>
+    </Router>
+  );
+}
+
+export default App;
+
