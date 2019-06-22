3317602ebf9047efaca23ddade62cfbd072de0fc
yuhan0927 <1138767969@qq.com>
2019-06-22 22:07:14 +0800

    react-dynamicfiled-redux-count

---
+++ react/react-dynamicfiled-redux-count/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import DynamicFiled from './DynamicField'
+// import './App.css';
+import 'antd/dist/antd.css'
+
+function App() {
+  return (
+    <div>
+      < DynamicFiled/>
+    </div>
+  )
+}
+
+export default App;
