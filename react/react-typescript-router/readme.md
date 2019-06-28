cd5c37301dd31306f8fcaa449e3eb7b25830f2d8
yuhan0927 <1138767969@qq.com>
2019-06-28 18:06:55 +0800

    react-typescript-router

---
+++ react/react-typescript-router/readme.md
@@ -0,0 +1,7 @@
+1. npm init -y
+2. yarn add typescript awesome-typescript-loader -D
+3. yarn add react react-router-dom react-dom
+4. yarn add @types/react @types/react-router @types/react-router-dom @types/react-dom -D
+5. yarn add webpack webpack-cli webpack-dev-server babel-core babel-cli babel-preset-env css-loader style-loader url-loader file-loader -D
+6. yarn add html-webpack-plugin mini-css-extract-plugin -D
+7. yarn add bootstrap
---
+++ react/react-typescript-router/src/api/member/mockData.ts
@@ -0,0 +1,13 @@
+import { MemberEntity } from '../../model'
+export const members:MemberEntity[] = [
+    {
+        id: 53435,
+        login: "fsdfsdfsdfs",
+        avatar_url: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561726219129&di=2df59a4ea913067527563338739f8936&imgtype=jpg&src=http%3A%2F%2Fimg3.imgtn.bdimg.com%2Fit%2Fu%3D2213042623%2C807394933%26fm%3D214%26gp%3D0.jpg"
+    },
+    {
+        id:42,
+        login:"dgdsgfd",
+        avatar_url: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561726249496&di=97ccd3d9f60ecc822a7198e3a425b4d9&imgtype=jpg&src=http%3A%2F%2Fimg4.imgtn.bdimg.com%2Fit%2Fu%3D3789069399%2C1600775213%26fm%3D214%26gp%3D0.jpg"
+    }
+]
